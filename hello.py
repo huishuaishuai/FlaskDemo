8a260b9aaa3bfc912d4b687f0627603e8ecbdd0b
JeanHui <sshui0316@gmail.com>
2017-08-10 22:31:11 +0800

    动态输入

--- hello.py
+++ hello.py
@@ -11,5 +11,10 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return 'Hello, %s' % name
+
+
 if __name__ == '__main__':
     app.run(debug=True)
