c8720b3de05a67916a75612c37de92e765292cdb
JeanHui <sshui0316@gmail.com>
2017-08-12 10:25:45 +0800

    模板响应！

--- hello.py
+++ hello.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return 'Hello, %s' % name
-
+    return render_template('user.html', name=name)
+    # return '<h1>Hello, %s</h1>' % name
 
 if __name__ == '__main__':
     app.run(debug=True)
